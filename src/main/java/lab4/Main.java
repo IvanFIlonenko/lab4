463ee2f000041d2feebcd91e5775cc350efcc53a
Ivan <Ivan Filonenko>
2019-11-01 00:26:44 +0300

    Scripted auto-commit on change (2019-11-01 00:26:44) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -1,41 +1,45 @@
-package lab4;
-
-import akka.actor.AbstractActor;
-import akka.actor.AbstractActor.Receive;
-import akka.actor.ActorRef;
+import akka.NotUsed;
 import akka.actor.ActorSystem;
-import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
-class PrintMyActorRefActor extends AbstractActor {
-    static Props props() {
-        return Props.create(PrintMyActorRefActor.class, PrintMyActorRefActor::new);
+import java.util.concurrent.CompletionStage;
+
+public class Main extends AllDirectives {
+
+    public static void main(String[] args) throws Exception {
+        // boot up server using the route as defined below
+        ActorSystem system = ActorSystem.create("routes");
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        //In order to access all directives we need an instance where the routes are define.
+        HttpServerMinimalExampleTest app = new HttpServerMinimalExampleTest();
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
+                ConnectHttp.toHost("localhost", 8080), materializer);
+
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read(); // let it run until user presses return
+
+        binding
+                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
     }
 
-    @Override
-    public Receive createReceive() {
-        return receiveBuilder()
-                .matchEquals(
-                        "printit",
-                        p -> {
-                            ActorRef secondRef = getContext().actorOf(Props.empty(), "second-actor");
-                            System.out.println("Second: " + secondRef);
-                        })
-                .build();
-    }
-}
-public class Main {
-    public static void main(String[] args) throws java.io.IOException {
-        ActorSystem system = ActorSystem.create("testSystem");
-
-        ActorRef firstRef = system.actorOf(PrintMyActorRefActor.props(), "first-actor");
-        System.out.println("First: " + firstRef);
-        firstRef.tell("printit", ActorRef.noSender());
-
-        System.out.println(">>> Press ENTER to exit <<<");
-        try {
-            System.in.read();
-        } finally {
-            system.terminate();
-        }
+    private Route createRoute() {
+        return concat(
+                path("hello", () ->
+                        get(() ->
+                                complete("<h1>Say hello to akka-http</h1>"))));
     }
 }
